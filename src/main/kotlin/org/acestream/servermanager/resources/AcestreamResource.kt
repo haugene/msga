1243aa8910da079f787a10886d30db2fb306c297
Kristian Haugene <kristian.haugene@greenbird.com>
2019-10-16 20:30:04 +0200

    Running status of Acestream

--- src/main/kotlin/org/acestream/servermanager/resources/AcestreamResource.kt
+++ src/main/kotlin/org/acestream/servermanager/resources/AcestreamResource.kt
@@ -6,7 +6,9 @@ import org.acestream.servermanager.services.StreamTestService
 import org.acestream.servermanager.utils.CommandLineHelper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @RestController
 @RequestMapping("acestream")
@@ -18,7 +20,13 @@ class AcestreamResource(
 
     @GetMapping("start")
     fun startAcestreamServer(): String {
+        if (cliHelper.acestreamIsRunning()) throw WebApplicationException(Response.Status.CONFLICT)
         return cliHelper.startAcestreamServer()
+    }
+
+    @GetMapping("running")
+    fun isOpenVpnRunning(): Map<String, Boolean> {
+        return mapOf(Pair("running", cliHelper.acestreamIsRunning()))
     }
 
     @PostMapping("testStreams")
--- src/main/kotlin/org/acestream/servermanager/utils/CommandLineHelper.kt
+++ src/main/kotlin/org/acestream/servermanager/utils/CommandLineHelper.kt
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.zeroturnaround.exec.ProcessExecutor
+import org.zeroturnaround.exec.StartedProcess
 import org.zeroturnaround.exec.stream.slf4j.Slf4jStream
 import java.io.File
 import javax.ws.rs.client.ClientBuilder
@@ -18,12 +19,14 @@ import javax.ws.rs.core.MediaType
 
 @Service
 class CommandLineHelper(
-    @Autowired val settingsService: SettingsService
+        @Autowired val settingsService: SettingsService
 ) {
 
     companion object {
         private val logger = LoggerFactory.getLogger(CommandLineHelper::class.java)
     }
+
+    var acestreamProcess: StartedProcess? = null
 
     fun startAcestreamServer(): String {
         val logFile = File("/var/log/acestream/server1.log")
@@ -37,20 +40,21 @@ class CommandLineHelper(
         }
 
 
-        val process = ProcessExecutor()
-            .command(
-                "/opt/acestream/acestreamengine",
-                "--client-console",
-                "--live-cache-type memory",
-                "--live-cache-size ${settings().liveCacheSize * 1024 * 1024}",
-                "--port=${settings().aceServerPort}",
-                "--live-buffer ${settings().liveBufferSeconds}",
-                "--http-port=30000"
-            )
-            .redirectOutput(logFile.outputStream())
-            .start()
+        acestreamProcess = ProcessExecutor()
+                .command(
+                        "/opt/acestream/acestreamengine",
+                        "--client-console",
+                        "--live-cache-type memory",
+                        "--live-cache-size ${settings().liveCacheSize * 1024 * 1024}",
+                        "--port=${settings().aceServerPort}",
+                        "--live-buffer ${settings().liveBufferSeconds}",
+                        "--http-port=30000"
+                )
+                .redirectOutput(logFile.outputStream())
+                .start()
 
-        logger.info("AceStream is alive: ${process.process.isAlive}")
+
+        logger.info("AceStream is alive: ${acestreamIsRunning()}")
         return "OK"
     }
 
@@ -58,8 +62,8 @@ class CommandLineHelper(
         val regex = """openjdk version \"(.*)\"""".toRegex()
 
         val result = ProcessExecutor().command("java", "-version")
-            .readOutput(true).execute()
-            .outputUTF8()
+                .readOutput(true).execute()
+                .outputUTF8()
 
         return regex.find(result)!!.groupValues[1]
     }
@@ -68,8 +72,8 @@ class CommandLineHelper(
         val regex = """OpenVPN\s+(\d+\.\d+\.\d+)\s+x86_64""".toRegex()
 
         val result = ProcessExecutor().command("openvpn", "--version")
-            .readOutput(true).execute()
-            .outputUTF8()
+                .readOutput(true).execute()
+                .outputUTF8()
 
         return regex.find(result)!!.groupValues[1]
     }
@@ -78,8 +82,8 @@ class CommandLineHelper(
         val regex = """version:\s+(\d+\.\d+\.\d+)\s+revision:\s+(\d+)""".toRegex()
 
         val result = ProcessExecutor().command("/opt/acestream/acestreamengine", "-v")
-            .readOutput(true).execute()
-            .outputUTF8()
+                .readOutput(true).execute()
+                .outputUTF8()
 
         val (version, revision) = regex.find(result)!!.destructured
         return AcestreamInfo(version, revision)
@@ -95,81 +99,81 @@ class CommandLineHelper(
 
         logger.info("Starting OpenVPN")
         ProcessExecutor()
-            .command(
-                "openvpn",
-                "--writepid", "/opt/openvpn/pid.txt",
-                "--status", "/opt/openvpn/status.txt", "5",
-                "--log", "/opt/openvpn/client.log",
-                //"--log-append", "/opt/openvpn/client.log", // Append instead of replace on each run
-                "--script-security", "2",
-                "--down", "/opt/openvpn/killAcestream.sh",
-                "--config", "/opt/openvpn/Berlin.ovpn",
-                "--daemon"
-            )
-            .redirectOutput(Slf4jStream.of(logger).asWarn())
-            .exitValueNormal()
-            .execute()
+                .command(
+                        "openvpn",
+                        "--writepid", "/opt/openvpn/pid.txt",
+                        "--status", "/opt/openvpn/status.txt", "5",
+                        "--log", "/opt/openvpn/client.log",
+                        //"--log-append", "/opt/openvpn/client.log", // Append instead of replace on each run
+                        "--script-security", "2",
+                        "--down", "/opt/openvpn/killAcestream.sh",
+                        "--config", "/opt/openvpn/Berlin.ovpn",
+                        "--daemon"
+                )
+                .redirectOutput(Slf4jStream.of(logger).asWarn())
+                .exitValueNormal()
+                .execute()
     }
 
     private fun setUsernameAndPassword() {
         val settings = settingsService.getSettings()
         File("/opt/openvpn/credentials.txt")
-            .writeText(settings.openvpnUsername + "\n" + settings.openvpnPassword)
+                .writeText(settings.openvpnUsername + "\n" + settings.openvpnPassword)
     }
 
     private fun tunDeviceExists() = File("/dev/net/tun").exists()
 
     private fun createTunDevice() {
         ProcessExecutor().command("mkdir", "-p", "/dev/net")
-            .redirectOutput(Slf4jStream.of(logger).asWarn())
-            .exitValueNormal()
-            .execute()
+                .redirectOutput(Slf4jStream.of(logger).asWarn())
+                .exitValueNormal()
+                .execute()
         ProcessExecutor().command("mknod", "/dev/net/tun", "c", "10", "200")
-            .redirectOutput(Slf4jStream.of(logger).asWarn())
-            .exitValueNormal()
-            .execute()
+                .redirectOutput(Slf4jStream.of(logger).asWarn())
+                .exitValueNormal()
+                .execute()
         ProcessExecutor().command("chmod", "0666", "/dev/net/tun")
-            .redirectOutput(Slf4jStream.of(logger).asWarn())
-            .exitValueNormal()
-            .execute()
+                .redirectOutput(Slf4jStream.of(logger).asWarn())
+                .exitValueNormal()
+                .execute()
     }
 
     fun testDns() {
         ProcessExecutor()
-            .command(
-                "dig",
-                "google.com",
-                "+short", // Only IP in response
-                "+time=1" // Set quicker timeout
-            )
-            .exitValueNormal()
-            .execute()
+                .command(
+                        "dig",
+                        "google.com",
+                        "+short", // Only IP in response
+                        "+time=1" // Set quicker timeout
+                )
+                .exitValueNormal()
+                .execute()
     }
 
     fun setDnsServers() {
         logger.info("Setting DNS records")
         File("/etc/resolv.conf")
-            .writeText(
-                settingsService.getSettings()
-                    .dnsServers.joinToString(separator = "\n", transform = { "nameserver $it" })
-            )
+                .writeText(
+                        settingsService.getSettings()
+                                .dnsServers.joinToString(separator = "\n", transform = { "nameserver $it" })
+                )
     }
 
     fun publicIp(): String? {
         return ClientBuilder.newClient()
-            .target("http://ipecho.net")
-            .path("plain")
-            .request(MediaType.APPLICATION_JSON_TYPE)
-            .get(String::class.java)
+                .target("http://ipecho.net")
+                .path("plain")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(String::class.java)
     }
 
     fun openPort(): Int {
         val piaClientId = RandomStringUtils.randomAlphanumeric(60)
         val response = ClientBuilder.newClient()
-            .target("http://209.222.18.222:2000")
-            .queryParam("client_id", piaClientId)
-            .request(MediaType.APPLICATION_JSON_TYPE)
-            .get(String::class.java)
+                .target("http://209.222.18.222:2000")
+                .queryParam("client_id", piaClientId)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(String::class.java)
 
         val port = ObjectMapper().readValue<JsonNode>(response).get("port").asText()
         logger.info("Got port $port from PIA")
@@ -187,12 +191,14 @@ class CommandLineHelper(
         if (!pidFile.exists()) return false
 
         val result = ProcessExecutor().command("ps", "-p", pidFile.readLines().first())
-            .redirectError(Slf4jStream.of(logger).asWarn())
-            .exitValueAny()
-            .execute()
+                .redirectError(Slf4jStream.of(logger).asWarn())
+                .exitValueAny()
+                .execute()
 
         return result.exitValue == 0
     }
+
+    fun acestreamIsRunning(): Boolean = acestreamProcess?.process?.isAlive ?: false
 
     fun configureIpTables() {
         // Returns something like "default via 172.20.0.1 dev eth0"
