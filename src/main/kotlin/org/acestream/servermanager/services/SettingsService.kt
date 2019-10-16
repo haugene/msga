294df38c32758e194778d4f4387068c702d3b312
Kristian Haugene <kristian.haugene@greenbird.com>
2019-10-16 20:08:47 +0200

    Example value

--- src/main/kotlin/org/acestream/servermanager/services/SettingsService.kt
+++ src/main/kotlin/org/acestream/servermanager/services/SettingsService.kt
@@ -41,7 +41,7 @@ class SettingsService {
                 aceServerPort = 25000,
                 liveBufferSeconds = 30,
                 configureLocalNetworkRoutes = false,
-                localNetworkRoutes = emptyList()
+                localNetworkRoutes = listOf("192.168.1.0/24")
         )
 
         writeSettings(settings)
