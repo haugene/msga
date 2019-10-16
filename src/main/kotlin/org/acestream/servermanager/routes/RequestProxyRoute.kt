6525406899a3f512e60085f93de24d19a0572971
Kristian Haugene <kristian.haugene@greenbird.com>
2019-10-16 19:01:49 +0200

    Better proxy

--- src/main/kotlin/org/acestream/servermanager/routes/RequestProxyRoute.kt
+++ src/main/kotlin/org/acestream/servermanager/routes/RequestProxyRoute.kt
@@ -4,18 +4,13 @@ import org.apache.camel.Exchange
 import org.apache.camel.Processor
 import org.apache.camel.spring.SpringRouteBuilder
 import org.springframework.stereotype.Component
-import java.lang.RuntimeException
+import java.net.URLEncoder
 
 @Component
 class RequestProxyRoute : SpringRouteBuilder() {
     override fun configure() {
         from("servlet:request?servletName=RequestProxyServlet")
             .routeId("request-proxy")
-            .process {
-                if (!it.`in`.getHeader("url", String::class.java).startsWith(prefix = "http")) {
-                    throw IllegalArgumentException("Proxy URL must start with \"http\"")
-                }
-            }
             .process(UrlProcessor())
             .toD("\${header.PROXY_TO_URL}?bridgeEndpoint=true")
             .process { it.`in`.headers["Access-Control-Allow-Origin"] = "*" }
@@ -23,8 +18,19 @@ class RequestProxyRoute : SpringRouteBuilder() {
 }
 
 class UrlProcessor: Processor {
-    override fun process(exchange: Exchange?) {
-        if (exchange == null) throw RuntimeException()
-        exchange.`in`.setHeader("PROXY_TO_URL", exchange.`in`.getHeader(Exchange.HTTP_QUERY, String::class.java).removePrefix("url="))
+    override fun process(exchange: Exchange) {
+        // Verify input
+        if (!exchange.`in`.getHeader("url", String::class.java).startsWith(prefix = "http")){
+            throw IllegalArgumentException("Proxy URL must start with \"http\"")
+        }
+
+        // URL-encode most of it, but keep protocol:// for Camel to know what's going on.
+        val httpQuery = exchange.`in`.getHeader(Exchange.HTTP_QUERY, String::class.java).removePrefix("url=")
+        val protocol = httpQuery.substringBefore("://")
+        val hostPathAndQuery = httpQuery.substringAfter("://")
+        val urlEncodedHostPathAndQuery = URLEncoder.encode(hostPathAndQuery, Charsets.UTF_8.name())
+
+        // Set the header to be used in toD endpoint
+        exchange.`in`.setHeader("PROXY_TO_URL", "$protocol://$urlEncodedHostPathAndQuery")
     }
 }
